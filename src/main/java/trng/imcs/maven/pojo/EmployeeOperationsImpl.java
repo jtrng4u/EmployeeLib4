2afdb81d7700902ec36ceb24c15351d57da5ab59
jtrng4u@gmail.com <jtrng4u@gmail.com>
2018-08-18 18:59:05 -0500

    Dev1 changes.

--- src/main/java/trng/imcs/maven/pojo/EmployeeOperationsImpl.java
+++ src/main/java/trng/imcs/maven/pojo/EmployeeOperationsImpl.java
@@ -7,20 +7,25 @@ public class EmployeeOperationsImpl implements EmployeeOperations {
 	List<Employee> employees;
 
 	public EmployeeOperationsImpl() {
-		employees.add(new Employee(123, "Raj"));
 		employees.add(new Employee(124, "David"));
-		employees.add(new Employee(125, "John"));
+		employees.add(new Employee(400, "John"));
 	}
 
 	public Employee getEmployee(int empId) {
 		for (Employee e : employees) {
 			if (e.getEmpNo() == empId) {
+				System.out.printn("Testing...");
 				return e;
 			}
 		}
+		
 		return null;
 	}
 
+	public void test() {
+	
+	}
+	
 	public void createEmployee(Employee emp) {
 		employees.add(emp);
 	}
